0df52a04f0858323ed3432c2cee9f9dfb8e8261c
Brandon Spears <bsp3ars@gmail.com>
2020-04-22 15:38:42 -0400

    Change submission email text

--- spec/mailers/publication_mailer_spec.rb
+++ spec/mailers/publication_mailer_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe PublicationMailer, type: :mailer do
       expect(mail.to).to eq([submitter.email_address, ENV['MAIL_SENDER']])
       expect(mail.from).to eq([ENV['MAIL_SENDER']])
       expect(mail.body.encoded).to match(book.work_title)
-      expect(mail.body.encoded).to match('Please include an address where I may return your publication')
+      expect(mail.body.encoded).to match('More information about the event is forthcoming')
     end
   end
 end
