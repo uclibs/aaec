6946dd15320a6dd79f33cc480a5134ee05f17fda
Brandon Spears <bsp3ars@gmail.com>
2020-07-21 14:18:59 -0400

    Make citations admin only

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -26,10 +26,14 @@ class AdminController < ApplicationController
   end
 
   def citations
-    all = (Artwork.all + Book.all + BookChapter.all + DigitalProject.all + Editing.all + Film.all + JournalArticle.all + MusicalScore.all + Photography.all + PhysicalMedium.all + Play.all + PublicPerformance.all + OtherPublication.all)
-    @college_array = []
-    (1..College.count).each do |i|
-      @college_array << [i, all.select { |p| p.college_ids.include? i }.group_by(&:uc_department)]
+    if session[:admin]
+      all = (Artwork.all + Book.all + BookChapter.all + DigitalProject.all + Editing.all + Film.all + JournalArticle.all + MusicalScore.all + Photography.all + PhysicalMedium.all + Play.all + PublicPerformance.all + OtherPublication.all)
+      @college_array = []
+      (1..College.count).each do |i|
+        @college_array << [i, all.select { |p| p.college_ids.include? i }.group_by(&:uc_department)]
+      end
+    else
+      redirect_to publications_path
     end
   end
 
