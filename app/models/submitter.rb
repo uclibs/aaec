22c39a15818302a52930cb9ac421b8dc3838d341
Janell-Huyck <huyckjl@ucmail.uc.edu>
2023-09-07 10:30:54 -0400

    fixed format-validation vulnerability for submitting phone number

--- app/models/submitter.rb
+++ app/models/submitter.rb
@@ -5,7 +5,7 @@ class Submitter < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :mailing_address, presence: true
-  validates :phone_number, presence: true, format: { with: /\d{3}-\d{3}-\d{4}/, message: 'Please use the format 111-111-1111' }
+  validates :phone_number, presence: true, format: { with: /\A\d{3}-\d{3}-\d{4}\z/, message: 'Please use the format 111-111-1111' }
   validates :email_address, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Please enter a valid email' }
 
   def self.to_csv
