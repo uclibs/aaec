225627af5782bab39640698e1c8702b0c0e49968
Janell-Huyck <huyckjl@ucmail.uc.edu>
2023-09-14 15:23:27 -0400

    rebase Gemfile.lock

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -103,7 +103,7 @@ require 'pagy/extras/bootstrap'
 
 # Instance variables
 # See https://ddnexus.github.io/pagy/api/pagy#instance-variables
-Pagy::VARS[:items] = 10 # default
+Pagy::DEFAULT[:items] = 10 # default
 
 # Other Variables
 # See https://ddnexus.github.io/pagy/api/pagy#other-variables
