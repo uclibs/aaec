ade17eb6b3ff0cee68b3aefc991a99205ed483a4
Janell-Huyck <huyckjl@ucmail.uc.edu>
2023-09-21 17:06:44 -0400

    expanded tests for publication_mailer

--- app/mailers/publication_mailer.rb
+++ app/mailers/publication_mailer.rb
@@ -2,12 +2,12 @@
 
 class PublicationMailer < ApplicationMailer
   helper MailerHelper
-  default from: -> { ENV.fetch('MAIL_SENDER', nil) }
 
   def publication_submit(submitter, publication)
     @submitter = submitter
     @publication = publication
-    receivers = "#{submitter.email_address}, #{ENV.fetch('MAIL_SENDER', nil)}"
-    mail(to: receivers, subject: 'Publication received for Artists, Authors, Editors & Composers')
+    default_sender = ENV.fetch('MAIL_SENDER')
+    receivers = "#{submitter.email_address}, #{default_sender}"
+    mail(to: receivers, from: default_sender, subject: 'Publication received for Artists, Authors, Editors & Composers')
   end
 end
