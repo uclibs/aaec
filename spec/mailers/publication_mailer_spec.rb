c93ca926e0b205666743dcd024e154beeb44fdcc
Janell-Huyck <huyckjl@ucmail.uc.edu>
2023-10-05 12:20:52 -0400

    fixed broken test

--- spec/mailers/publication_mailer_spec.rb
+++ spec/mailers/publication_mailer_spec.rb
@@ -9,9 +9,10 @@ RSpec.describe PublicationMailer, type: :mailer do
     it 'sends an email for a book' do
       book = FactoryBot.create(:book)
       mail = described_class.publication_submit(submitter, book).deliver_now
+      default_sender = ENV.fetch('MAIL_SENDER')[/<(.*?)>/, 1] # Extract email address from MAIL_SENDER
       expect(mail.subject).to eq('Publication received for Artists, Authors, Editors & Composers')
-      expect(mail.to).to eq([submitter.email_address, ENV.fetch('MAIL_SENDER', nil)])
-      expect(mail.from).to eq([ENV.fetch('MAIL_SENDER', nil)])
+      expect(mail.to).to eq([submitter.email_address, default_sender])
+      expect(mail.from).to eq([default_sender])
       expect(mail.body.encoded).to match(book.work_title)
       expect(mail.body.encoded).to match("More information about this year's event is forthcoming")
       expect(mail.body.encoded).to include('<html>').once
