4429ac4a00af1e6f89f71ff1ac5a31183f4f90fc
Janell Huyck <huyckjl@ucmail.uc.edu>
2023-10-05 10:51:51 -0400

    217 feature branch error pages css fix (#235)
    * wip, broken rescue_from
    
    * error pages now being displayed dynamically
    
    * removed 422 and 500 pages
    
    * added test for missing coverage in admincontroller so that code coverage doesn't go down
    
    * returned default qa deploy branch back to qa
    
    ---------
    
    Co-authored-by: Sean Crowe <1069588+crowesn@users.noreply.github.com>

--- spec/controllers/admin_controller/admin_controller_csv_spec.rb
+++ spec/controllers/admin_controller/admin_controller_csv_spec.rb
@@ -3,20 +3,39 @@
 require 'rails_helper'
 
 RSpec.describe AdminController, type: :controller do
+  let(:common_params) { { 'controller_name' => 'other_publications' } }
+  let(:admin_session) { { 'admin' => true } }
+
   describe 'GET #csv' do
-    it 'returns a csv when admin' do
-      get(:csv, params: { :format => 'csv', 'controller_name' => 'other_publications' }, session: { 'admin' => true })
-      expect(response.status).to eq 200
+    context 'when the user is an admin' do
+      it 'returns a 200 status when requesting a CSV format' do
+        get(:csv, params: common_params.merge({ format: 'csv' }), session: admin_session)
+        expect(response.status).to eq(200)
+      end
+
+      it 'redirects when an invalid format is provided' do
+        get(:csv, params: common_params.merge({ format: 'html' }), session: admin_session)
+        expect(response).to redirect_to('/publications')
+      end
+
+      context 'when a StandardError is raised' do
+        before do
+          allow(OtherPublication).to receive(:to_csv).and_raise(StandardError)
+        end
+
+        it 'redirects with a notice' do
+          get(:csv, params: common_params.merge({ format: 'csv' }), session: admin_session)
+          expect(response).to redirect_to('/publications')
+          expect(flash[:notice]).to eq('Something went wrong while generating the CSV.')
+        end
+      end
     end
 
-    it 'redirects when invalid format and admin' do
-      get(:csv, params: { :format => 'html', 'controller_name' => 'other_publications' }, session: { 'admin' => true })
-      expect(response).to redirect_to('/publications')
-    end
-
-    it 'redirects when not admin but valid' do
-      get(:csv, params: { :format => 'csv', 'controller_name' => 'other_publications' })
-      expect(response).to redirect_to('/publications')
+    context 'when the user is not an admin' do
+      it 'redirects even if a valid format is provided' do
+        get(:csv, params: common_params.merge({ format: 'csv' }))
+        expect(response).to redirect_to('/publications')
+      end
     end
   end
 end
