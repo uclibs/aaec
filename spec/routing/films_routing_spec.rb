cf5863920c8864a547a13194d8d5af1d6ed18d1f
Janell-Huyck <huyckjl@ucmail.uc.edu>
2023-10-13 19:55:53 -0400

    corrected routing test contexts

--- spec/routing/films_routing_spec.rb
+++ spec/routing/films_routing_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe BookChaptersController, type: :routing do
+RSpec.describe FilmsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
       expect(get: '/films').to route_to('films#index')
--- spec/routing/physical_media_routing_spec.rb
+++ spec/routing/physical_media_routing_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe BookChaptersController, type: :routing do
+RSpec.describe PhysicalMediaController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
       expect(get: '/physical_media').to route_to('physical_media#index')
