96b6eb8f09948fc1ccaea7837f6d1210a19d12c4
Janell-Huyck <huyckjl@ucmail.uc.edu>
2023-10-19 10:22:05 -0400

    expanded multi_concern_interaction test

--- spec/requests/multi_concern_interaction_spec.rb
+++ spec/requests/multi_concern_interaction_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
 RSpec.describe 'Authenticity and Date Checks', type: :request do
   before do
     allow_any_instance_of(ApplicationController).to receive(:verify_authenticity_token) do |instance|
@@ -11,11 +12,24 @@ RSpec.describe 'Authenticity and Date Checks', type: :request do
   it 'redirects to closed page when app is out of date and token is invalid' do
     allow(ENV).to receive(:fetch).with('EXPIRATION_DATE').and_return('2000-01-01')
 
+    warn_count = 0
+    allow(Rails.logger).to receive(:warn) do |arg|
+      warn_count += 1 if arg.match?(/InvalidAuthenticityToken occurred:/)
+    end
+
     get publications_path, headers: { '_csrf_token' => 'invalid_token' }
 
+    expect(response).to have_http_status(:redirect) # 302
     expect(response).to redirect_to(root_path)
     follow_redirect!
 
+    expect(warn_count).to eq(1)
+    expect(response).to have_http_status(:redirect) # 302
     expect(response).to redirect_to('/pages/closed')
+
+    # Reset the received count for :warn calls on Rails.logger
+    allow(Rails.logger).to receive(:warn).once
+
+    expect(warn_count).to eq(1)
   end
 end
