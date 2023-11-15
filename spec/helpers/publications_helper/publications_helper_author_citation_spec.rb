e065cf256ceca1adf20cfdf5bc50c4863fb60a1a
Janell-Huyck <huyckjl@ucmail.uc.edu>
2023-11-15 18:25:26 -0500

    additional test for coveralls failure

--- spec/helpers/publications_helper/publications_helper_author_citation_spec.rb
+++ spec/helpers/publications_helper/publications_helper_author_citation_spec.rb
@@ -22,11 +22,19 @@ RSpec.describe PublicationsHelper, type: :helper do
       end
     end
 
-    context 'when there are multiple authors' do
+    context 'when there are two authors' do
       let(:publication) { double(author_first_name: %w[John Jane], author_last_name: %w[Doe Roe]) }
 
       it 'returns all authors in citation format' do
         expect(helper.author_citation(publication)).to eq('Doe, John and Jane Roe')
+      end
+    end
+
+    context 'when there are more than two authors' do
+      let(:publication) { double(author_first_name: %w[John Jane Mary Jo], author_last_name: %w[Doe Roe Wallace Smith]) }
+
+      it 'returns all authors in citation format' do
+        expect(helper.author_citation(publication)).to eq('Doe, John, Roe, Jane, Wallace, Mary and Jo Smith')
       end
     end
 
