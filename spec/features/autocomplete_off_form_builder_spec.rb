ea0996da083c28bb2dbf3b189ca1f2e658a35638
Janell-Huyck <huyckjl@ucmail.uc.edu>
2023-11-16 17:18:06 -0500

    removed test expectation not modified by tested builder

--- spec/features/autocomplete_off_form_builder_spec.rb
+++ spec/features/autocomplete_off_form_builder_spec.rb
@@ -6,7 +6,6 @@ RSpec.feature 'AutocompleteOffFormBuilder', type: :feature do
   scenario 'renders text field with autocomplete off' do
     visit root_path
     expect(page).to have_selector("input[autocomplete='off']")
-    expect(page).to have_selector("select[autocomplete='off']")
 
     visit manage_path
     expect(page).to have_selector("input[autocomplete='off']")
