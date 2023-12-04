1617de88b61aeaf1f3a40de9c48ee96c7aa15336
Janell Huyck <huyckjl@ucmail.uc.edu>
2023-12-04 16:48:43 -0500

    Move code to toggle college 'other' field to own file (#298)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,7 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require bootstrap-tooltip-initialization
+//= require toggle_college_other_field
 
 function addAuthor(type, count)    {
     var count = count || 1;
@@ -44,33 +45,3 @@ function addAuthor(type, count)    {
 function removeAuthor(idToDelete)     {
     document.getElementById(idToDelete).outerHTML = '';
 }
-
-$(document).on('turbolinks:load', function() {
-    if (window.location.pathname.includes("/edit") || window.location.pathname.includes("/new")) {
-        otherChkbox = document.querySelector('[id$="_college_ids_16"]');
-        otherField = document.querySelector('[id$="_other_college"]');
-        otherGroup = document.querySelector('[id$="other_college_group"]');
-        tempStorage = '';
-
-        if (otherField.value != "") {
-            otherChkbox.checked = true;
-        }
-        else    {
-            otherGroup.classList.toggle("d-none");
-        }
-
-        otherChkbox.onclick = function() {
-            if (otherChkbox.checked) {
-                otherGroup.classList.toggle("d-none");
-                    if (tempStorage != '') {
-                        otherField.value = tempStorage;
-                    }
-                }
-            else    {
-                otherGroup.classList.toggle("d-none");
-                tempStorage = otherField.value;
-                otherField.value = '';
-            } 
-        }
-    }
-})
