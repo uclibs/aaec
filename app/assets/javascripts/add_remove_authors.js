2442f1b4924575ea739a68d7762d5f28ce1b25a5
Janell-Huyck <huyckjl@ucmail.uc.edu>
2023-12-07 13:40:28 -0500

    moved changes to add_remove_authors.js file

--- app/assets/javascripts/add_remove_authors.js
+++ app/assets/javascripts/add_remove_authors.js
@@ -1,23 +1,57 @@
-function addAuthor(type, count)    {
-  var count = count || 1;
-  var newAuthor = document.createElement("div");
-  var firstI = document.createElement("div");
-  var lastI = document.createElement("div");
-  var deleteB = document.createElement("div");
-  newAuthor.setAttribute("class", "form-row");
-  firstI.setAttribute("class", "col-md-5");
-  lastI.setAttribute("class", "col-md-5");
-  deleteB.setAttribute("class", "col-md-2");
-  firstI.innerHTML = `<input type="text" name="${type}[author_first_name][]" id="${type}_author_first_name_" required="required" class="form-control form-group">`;
-  lastI.innerHTML = `<input type="text" name="${type}[author_last_name][]" id="${type}_author_last_name_" required="required" class="form-control form-group">`;
-  deleteB.innerHTML = `<button name="button" type="button" onclick="removeAuthor('added_${count}');" class="form-control form-group bg-danger text-white">Remove Author</button>`;
-  newAuthor.setAttribute("id", "added_" + count);
-  newAuthor.appendChild(firstI);
-  newAuthor.appendChild(lastI);
-  newAuthor.appendChild(deleteB);
+function addAuthor(type, count) {
+  const newAuthorId = `added_${count}`;
+  const newAuthor = createAuthorElement(type, newAuthorId);
   document.getElementById("author_group").appendChild(newAuthor);
-  count++;
-  document.getElementById("add_author_btn").setAttribute("onClick", `addAuthor('${type}', ${count})`);
+  updateAddAuthorButton(type, count + 1);
+}
+
+function createAuthorElement(type, id) {
+  const newAuthor = createElementWithAttributes("div", { class: "form-row", id });
+  const firstNameInput = createInput(type, "author_first_name");
+  const lastNameInput = createInput(type, "author_last_name");
+  const deleteButton = createDeleteButton(id);
+
+  newAuthor.appendChild(firstNameInput);
+  newAuthor.appendChild(lastNameInput);
+  newAuthor.appendChild(deleteButton);
+  return newAuthor;
+}
+
+function createInput(type, name) {
+  const wrapper = createElementWithAttributes("div", { class: "col-md-5" });
+  const input = createElementWithAttributes("input", {
+      type: "text",
+      name: `${type}[${name}][]`,
+      id: `${type}_${name}_`,
+      required: "required",
+      class: "form-control form-group"
+  });
+  wrapper.appendChild(input);
+  return wrapper;
+}
+
+function createDeleteButton(id) {
+  const wrapper = createElementWithAttributes("div", { class: "col-md-2" });
+  const button = createElementWithAttributes("button", {
+      name: "button",
+      type: "button",
+      class: "form-control form-group bg-danger text-white"
+  });
+  button.textContent = "Remove Author";
+  button.addEventListener("click", () => removeAuthor(id));
+  wrapper.appendChild(button);
+  return wrapper;
+}
+
+function createElementWithAttributes(tag, attributes) {
+  const element = document.createElement(tag);
+  Object.entries(attributes).forEach(([key, value]) => element.setAttribute(key, value));
+  return element;
+}
+
+function updateAddAuthorButton(type, count) {
+  const addButton = document.getElementById("add_author_btn");
+  addButton.setAttribute("onClick", `addAuthor('${type}', ${count})`);
 }
 
 function removeAuthor(idToDelete)     {
