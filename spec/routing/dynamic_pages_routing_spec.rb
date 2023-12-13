0722d4781bb8577b7f64389554bf7910c3a52dae
Janell-Huyck <huyckjl@ucmail.uc.edu>
2023-12-13 15:55:20 -0500

    refactored dynamic_pages_routing test to iterate through all allowed pages

--- spec/routing/dynamic_pages_routing_spec.rb
+++ spec/routing/dynamic_pages_routing_spec.rb
@@ -4,24 +4,14 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :routing do
   describe 'routing' do
-    it 'routes to #show with about as parameter' do
-      expect(get: '/pages/about').to route_to('pages#show', page: 'about')
-    end
-
-    it 'routes to #show with contact as parameter' do
-      expect(get: '/pages/contact').to route_to('pages#show', page: 'contact')
-    end
-
-    it 'routes to #show with faq as parameter' do
-      expect(get: '/pages/faq').to route_to('pages#show', page: 'faq')
+    PagesController::ALLOWED_PAGES.each do |page|
+      it "routes to #show with #{page} as parameter" do
+        expect(get: "/pages/#{page}").to route_to('pages#show', page: page)
+      end
     end
 
     it 'does not route to #show without a page parameter' do
       expect(get: '/pages/').not_to be_routable
-    end
-
-    it 'routes to #closed' do
-      expect(get: '/pages/closed').to route_to('pages#show', page: 'closed')
     end
   end
 end
