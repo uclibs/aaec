ad4632323e290a637132db9ed925599c383bcd1e
Janell Huyck <huyckjl@ucmail.uc.edu>
2024-01-29 17:01:06 -0500

    296 - Refactor show/hide 'other' college field (#310)
    * rebasing - moved toggle_college_other_field change to its own file
    
    * Move publications colleges to partial, clean up html alignment on publication forms

--- app/assets/javascripts/toggle_college_other_field.js
+++ app/assets/javascripts/toggle_college_other_field.js
@@ -1,29 +1,34 @@
+// Show or hide the other college text field based on the checkbox
+// The field is hidden by default, and the checkbox is checked if the field has a value
+// If the checkbox is unchecked, the field's value is stored in tempStorage and the field is cleared
 $(document).on('turbolinks:load', function() {
-  if (window.location.pathname.includes("/edit") || window.location.pathname.includes("/new")) {
-      otherChkbox = document.querySelector('[id$="_college_ids_16"]');
-      otherField = document.querySelector('[id$="_other_college"]');
-      otherGroup = document.querySelector('[id$="other_college_group"]');
-      tempStorage = '';
+    if (window.location.pathname.includes("/edit") || window.location.pathname.includes("/new")) {
+        setupOtherCollegeCheckbox();
+    }
+});
 
-      if (otherField.value != "") {
-          otherChkbox.checked = true;
-      }
-      else    {
-          otherGroup.classList.toggle("d-none");
-      }
+function setupOtherCollegeCheckbox() {
+    const otherChkbox = document.querySelector('[id$="_college_ids_16"]');
+    const otherField = document.querySelector('[id$="_other_college"]');
+    const otherGroup = document.querySelector('[id$="other_college_group"]');
 
-      otherChkbox.onclick = function() {
-          if (otherChkbox.checked) {
-              otherGroup.classList.toggle("d-none");
-                  if (tempStorage != '') {
-                      otherField.value = tempStorage;
-                  }
-              }
-          else    {
-              otherGroup.classList.toggle("d-none");
-              tempStorage = otherField.value;
-              otherField.value = '';
-          } 
-      }
-  }
-})
+    // Initialize or use existing tempStorage
+    otherField.tempStorage = otherField.tempStorage || '';
+
+    // Set initial state based on otherField's value
+    otherChkbox.checked = otherField.value !== "";
+    otherGroup.classList.toggle("d-none", !otherChkbox.checked);
+
+    // Event listener for the checkbox
+    otherChkbox.addEventListener('click', () => {
+        otherGroup.classList.toggle("d-none");
+        if (otherChkbox.checked) {
+            if (otherField.tempStorage !== '') {
+                otherField.value = otherField.tempStorage;
+            }
+        } else {
+            otherField.tempStorage = otherField.value;
+            otherField.value = '';
+        }
+    });
+}
