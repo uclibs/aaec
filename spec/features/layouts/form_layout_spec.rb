0fac7d398f8113e6a9e2b725c32c9bf0a1bc1527
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-03-05 19:10:03 -0500

    Text horizontal alignment of items

--- spec/features/layouts/form_layout_spec.rb
+++ spec/features/layouts/form_layout_spec.rb
@@ -37,6 +37,40 @@ describe 'Form Layout Test', type: :feature, js: true do
   def mobile_has_one_column
     # Mobile view: Expect items to take full container width, indicating a one-column layout
     resize_window_to_mobile
+
+    verify_one_column_widths
+    verify_one_column_heights
+  end
+
+  def tablet_has_two_columns
+    # Tablet view: Expect items to take about half the container width, indicating a two-column layout
+    # The delete button on author form-items will be on a separate line so not counted in the width.
+    resize_window_to_tablet
+
+    verify_two_column_item_widths
+    verify_two_column_item_heights
+  end
+
+  def desktop_has_two_columns
+    # Desktop view: Expect items to take half container width, indicating a two-column layout
+    # There is no delete button, so it is not counted in the width.
+    resize_window_to_desktop
+
+    verify_two_column_item_widths
+    verify_two_column_item_heights
+  end
+
+  def desktop_has_three_columns
+    # Desktop view: Each form-item should take up half of the
+    # container width, minus the width of the delete button
+    resize_window_to_desktop
+    click_button('Add Author')
+
+    verify_three_column_item_widths
+    verify_three_column_item_heights
+  end
+
+  def verify_one_column_widths
     expected_item_width = container_width - 10 # Subtract padding
 
     all('.form-item').each do |item|
@@ -45,10 +79,13 @@ describe 'Form Layout Test', type: :feature, js: true do
     end
   end
 
-  def tablet_has_two_columns
-    # Tablet view: Expect items to take about half the container width, indicating a two-column layout
-    # The delete button on author form-items will be on a separate line so not counted in the width.
-    resize_window_to_tablet
+  def verify_one_column_heights
+    heights = retrieve_item_offsets
+
+    expect(heights[0]).not_to eq(heights[1])
+  end
+
+  def verify_two_column_item_widths
     expected_item_width = calculate_expected_item_width(intake_field_count: 2, gap_count: 1)
 
     all('.form-item').each do |item|
@@ -58,22 +95,13 @@ describe 'Form Layout Test', type: :feature, js: true do
     end
   end
 
-  def desktop_has_two_columns
-    # Desktop view: Expect items to take half container width, indicating a two-column layout
-    # There is no delete button, so it is not counted in the width.
-    resize_window_to_desktop
-    expected_item_width = calculate_expected_item_width(intake_field_count: 2, gap_count: 1)
-
-    all('.form-item').each do |item|
-      item_width = page.evaluate_script('arguments[0].offsetWidth', item.native).to_i
-      expect(item_width).to be_within(5).of(expected_item_width)
-    end
+  def verify_two_column_item_heights
+    heights = retrieve_item_offsets
+    expect(heights[0]).to eq(heights[1])
+    expect(heights[1]).not_to eq(heights[2])
   end
 
-  def desktop_has_three_columns
-    # Desktop view: Each form-item should take up half of the
-    # container width, minus the width of the delete button
-    resize_window_to_desktop
+  def verify_three_column_item_widths
     expected_item_width = calculate_expected_item_width(intake_field_count: 2, gap_count: 2)
 
     all('.form-item').each do |item|
@@ -81,6 +109,23 @@ describe 'Form Layout Test', type: :feature, js: true do
 
       expect(item_width).to be_within(5).of(expected_item_width)
     end
+  end
+
+  def verify_three_column_item_heights
+    heights = retrieve_item_offsets
+    puts "heights: #{heights}"
+    expect(heights[0..2].uniq.size).to eq(1)
+    expect(heights[2]).not_to eq(heights[3])
+  end
+
+  # Returns an array of the heights (y-values) of the bottoms of each of the children
+  # for the form-container.
+  # This is used to determine if the form-items are on the same line or not.
+  # I am using the bottom because the heights of the items are not consistent
+  # and they are aligned by their bottoms.
+  def retrieve_item_offsets
+    script = "  Array.from(document.querySelector('.form-container').children).map(function(child) {return child.offsetTop + child.clientHeight; });"
+    page.evaluate_script(script)
   end
 
   def calculate_expected_item_width(intake_field_count:, gap_count: 0, space_between_items: 10)
