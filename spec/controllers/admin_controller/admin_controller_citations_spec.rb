dab8427625465ff38bd4fd8f0800441f8a026124
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-03-07 13:40:34 -0500

    Rubocop fix

--- spec/controllers/admin_controller/admin_controller_citations_spec.rb
+++ spec/controllers/admin_controller/admin_controller_citations_spec.rb
@@ -21,59 +21,59 @@ RSpec.describe AdminController, type: :controller do
         get :citations
 
         department_order = assigns(:college_array).find { |element| element[0] == 1 }[1].keys
-        expect(department_order).to eq(['Chemistry', 'Mathematics', 'Physics'])
+        expect(department_order).to eq(%w[Chemistry Mathematics Physics])
       end
     end
 
     context 'when publications are present in a department' do
-    context 'with multiple authors where the first author is the same' do
-      before do
-        session[:admin] = true
-        mock_college = double('College', id: 1)
-        allow(College).to receive(:find_each).and_yield(mock_college)
+      context 'with multiple authors where the first author is the same' do
+        before do
+          session[:admin] = true
+          mock_college = double('College', id: 1)
+          allow(College).to receive(:find_each).and_yield(mock_college)
 
-        # Publications with the same first author, different second authors
-        mock_publication1 = double('Publication',
-                                   college_ids: [1], uc_department: 'Literature',
-                                   author_last_name: ['Smith', 'Anderson'], author_first_name: ['John', 'Michael'], work_title: 'Book One')
-        mock_publication2 = double('Publication',
-                                   college_ids: [1], uc_department: 'Literature',
-                                   author_last_name: ['Smith', 'Bell'], author_first_name: ['John', 'Peter'], work_title: 'Book Two')
+          # Publications with the same first author, different second authors
+          mock_publication1 = double('Publication',
+                                     college_ids: [1], uc_department: 'Literature',
+                                     author_last_name: %w[Smith Anderson], author_first_name: %w[John Michael], work_title: 'Book One')
+          mock_publication2 = double('Publication',
+                                     college_ids: [1], uc_department: 'Literature',
+                                     author_last_name: %w[Smith Bell], author_first_name: %w[John Peter], work_title: 'Book Two')
 
-        allow(controller).to receive(:fetch_all_records).and_return([mock_publication1, mock_publication2])
+          allow(controller).to receive(:fetch_all_records).and_return([mock_publication1, mock_publication2])
+        end
+
+        it 'correctly sorts publications within departments by all authors' do
+          get :citations
+
+          sorted_publications_literature = assigns(:college_array).find { |element| element[0] == 1 }[1]['Literature']
+          expect(sorted_publications_literature.map(&:work_title)).to eq(['Book One', 'Book Two'])
+        end
       end
+      context 'with a single author having multiple publications' do
+        before do
+          session[:admin] = true
+          mock_college = double('College', id: 1)
+          allow(College).to receive(:find_each).and_yield(mock_college)
 
-      it 'correctly sorts publications within departments by all authors' do
-        get :citations
+          # Multiple publications by the same author
+          mock_publication1 = double('Publication',
+                                     college_ids: [1], uc_department: 'Science',
+                                     author_last_name: ['Curie'], author_first_name: ['Marie'], work_title: 'Research Findings')
+          mock_publication2 = double('Publication',
+                                     college_ids: [1], uc_department: 'Science',
+                                     author_last_name: ['Curie'], author_first_name: ['Marie'], work_title: 'Early Experiments')
 
-        sorted_publications_literature = assigns(:college_array).find { |element| element[0] == 1 }[1]['Literature']
-        expect(sorted_publications_literature.map(&:work_title)).to eq(['Book One', 'Book Two'])
+          allow(controller).to receive(:fetch_all_records).and_return([mock_publication1, mock_publication2])
+        end
+
+        it 'sorts publications by the same author within departments by work title' do
+          get :citations
+
+          sorted_publications_science = assigns(:college_array).find { |element| element[0] == 1 }[1]['Science']
+          expect(sorted_publications_science.map(&:work_title)).to eq(['Early Experiments', 'Research Findings'])
+        end
       end
-    end
-    context 'with a single author having multiple publications' do
-      before do
-        session[:admin] = true
-        mock_college = double('College', id: 1)
-        allow(College).to receive(:find_each).and_yield(mock_college)
-
-        # Multiple publications by the same author
-        mock_publication1 = double('Publication',
-                                   college_ids: [1], uc_department: 'Science',
-                                   author_last_name: ['Curie'], author_first_name: ['Marie'], work_title: 'Research Findings')
-        mock_publication2 = double('Publication',
-                                   college_ids: [1], uc_department: 'Science',
-                                   author_last_name: ['Curie'], author_first_name: ['Marie'], work_title: 'Early Experiments')
-
-        allow(controller).to receive(:fetch_all_records).and_return([mock_publication1, mock_publication2])
-      end
-
-      it 'sorts publications by the same author within departments by work title' do
-        get :citations
-
-        sorted_publications_science = assigns(:college_array).find { |element| element[0] == 1 }[1]['Science']
-        expect(sorted_publications_science.map(&:work_title)).to eq(['Early Experiments', 'Research Findings'])
-      end
-    end
     end
 
     context 'when logged in as a submitter' do
